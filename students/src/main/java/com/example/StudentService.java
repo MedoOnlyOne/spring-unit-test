673febbfeccd866c9a85060a27590d919f495f18
= <=>
2026-03-07 00:25:57 +0200

    use assetAll and assertTimeOut in Student project

--- students/src/main/java/com/example/StudentService.java
+++ students/src/main/java/com/example/StudentService.java
@@ -1,9 +1,19 @@
 package com.example;
 
+import java.time.Duration;
+
 public class StudentService {
     public Student findStudentById(int id) {
         if(id == 1)
+            return new Student(1, "Mohamed");
+        else if(id == 2)
+            return new Student(1, "Ahmed");
+        else if(id == 3)
             return new Student(1, "Medo");
         return null;
+    }
+    public Student findStudentByIdDelay() throws InterruptedException {
+        Thread.sleep(Duration.ofMillis(300));
+        return new Student(1, "Mohamed");
     }
 }
--- students/src/test/java/com/example/StudentServiceTest.java
+++ students/src/test/java/com/example/StudentServiceTest.java
@@ -2,15 +2,30 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StudentServiceTest {
     @Test
     public void testFindStudent() {
         StudentService studentService = new StudentService();
 
+        assertAll("Test Find Students By IDs",
+                ()-> assertEquals("Mohamed", studentService.findStudentById(1).getName()),
+                ()-> assertEquals("Ahmed", studentService.findStudentById(2).getName()),
+                ()-> assertEquals("Medo", studentService.findStudentById(3).getName())
+                );
+
         assertNull(studentService.findStudentById(10), "The student should be null");
         assertNotNull(studentService.findStudentById(1), "The student should not be null");
+
+    }
+
+    @Test
+    public void testTimeOut() {
+        StudentService studentService = new StudentService();
+
+        assertTimeout(Duration.ofMillis(500), () -> studentService.findStudentByIdDelay(), "Run Time should be in 500 ms range");
     }
 }
